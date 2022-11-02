8af0fe09ebec56a3fcf1ccd4789a711e8b7df7a2
Mitchell Simmons <s3530190@student.rmit.edu.au>
2022-11-03 10:07:04 +1100

    Fixed search suggestion now unique and modal only shows if valid choices

--- src/main/resources/js/search/connectModal.js
+++ src/main/resources/js/search/connectModal.js
@@ -10,6 +10,48 @@ function showResults() {
     const modal_overlay = $('.modal-overlay');
     const modal = $('.results-modal');
 
+    const checkbox_operatorName = $('#operator-name .checkbox');
+    const checkbox_loadNumber = $('#load-number .checkbox');
+    const checkbox_equipment = $('#equipment .checkbox');
+    const checkbox_runRecipe = $('#run-recipe .checkbox');
+
+    const search_operatorName = $('#operator-name .search');
+    const search_loadNumber = $('#load-number .search');
+    const search_equipment = $('#equipment .search');
+    const search_runRecipe = $('#run-recipe .search');
+
+    const operatorNameChecked = checkbox_operatorName.hasClass("checked");
+    const loadNumberChecked = checkbox_loadNumber.hasClass("checked");
+    const equipmentChecked = checkbox_equipment.hasClass("checked");
+    const runRecipeChecked = checkbox_runRecipe.hasClass("checked");
+
+    const operatorNameValue = search_operatorName.attr("data-value");
+    const loadNumberValue = search_loadNumber.attr("data-value");
+    const equipmentValue = search_equipment.attr("data-value");
+    const runRecipeValue = search_runRecipe.attr("data-value");
+
+    // If something checked, must have value
+    if (operatorNameChecked && !operatorNameValue) {
+        return;
+    }
+
+    if (loadNumberChecked && !loadNumberValue) {
+        return;
+    }
+
+    if (equipmentChecked && !equipmentValue) {
+        return;
+    }
+
+    if (runRecipeChecked && !runRecipeValue) {
+        return;
+    }
+
+    // If nothing checked, return
+    if (!operatorNameChecked && !loadNumberChecked && !equipmentChecked && !runRecipeChecked) {
+        return;
+    }
+
     modal_overlay.css('display', 'block');
     modal_overlay.addClass("modal-overlay-opacity-anim");
 
