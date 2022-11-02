22121c3a86ed7ec6111b251a5e40cfc707c56508
Mitchell Simmons <s3530190@student.rmit.edu.au>
2022-11-02 14:43:20 +1100

    Update IndexController

--- src/main/java/group08/boeing/index/IndexController.java
+++ src/main/java/group08/boeing/index/IndexController.java
@@ -4,10 +4,15 @@ import io.javalin.http.Handler;
 import java.util.Map;
 import java.util.HashMap;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import group08.boeing.util.FileUtil;
 
 public class IndexController {
     public static final String HTML_REQUEST = "/";
+    public static final String JSON_POST_REQUEST = "/upload";
     public static final String TARGET_HTML_FILE_PATH = "/html/index.html";
     // public static final String RESOURCE_HEADER_HTML_FILE_PATH = "./src/main/resources/html/common/header.html";
 
@@ -17,5 +22,10 @@ public class IndexController {
         Map<String, Object> model = new HashMap<>();
         // model.put("header", headerHtml);
         ctx.render(TARGET_HTML_FILE_PATH, model);
+    };
+
+    public static Handler parseJSON = ctx -> {
+        String str = ctx.body();
+        System.out.println(ctx.body());
     };
 }
