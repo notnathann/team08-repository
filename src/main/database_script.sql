b2edc821a29fa92c5955e2435a4df8f1046659eb
Nathan Nguyen <103355606+notnathann@users.noreply.github.com>
2022-11-02 15:52:58 +1100

    update bitch

--- src/main/database_script.sql
+++ src/main/database_script.sql
@@ -4,33 +4,33 @@ drop table if exists PART_INFORMATION;
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE RUN_DETAILS (
-	FileName           CHAR (10) NOT NULL,
-	FilePath           CHAR (10) NOT NULL,
-	LoadNumber         CHAR (10) NOT NULL,
-	Equipment            CHAR (10) NOT NULL,
-	RunRecipe           CHAR (10) NOT NULL,
-	RunStart            CHAR (10) NOT NULL,
-	RunEnd            CHAR (10) NOT NULL,
-	RunDuration           CHAR (10) NOT NULL,
-	FileLength            CHAR (10) NOT NULL,
-	OperatorName            CHAR (10) NOT NULL,
-	ExportControl              CHAR (10) NOT NULL,
+	FileName           TEXT NOT NULL,
+	FilePath           TEXT NOT NULL,
+	LoadNumber         TEXT NOT NULL,
+	Equipment          TEXT NOT NULL,
+	RunRecipe          TEXT NOT NULL,
+	RunStart           TEXT NOT NULL,
+	RunEnd             TEXT NOT NULL,
+	RunDuration        TEXT NOT NULL,
+	FileLength            TEXT NOT NULL,
+	OperatorName            TEXT NOT NULL,
+	ExportControl              TEXT NOT NULL,
     PRIMARY KEY (FileName)
 );
 
 CREATE TABLE PART_INFORMATION (
-    FileName           CHAR (10) NOT NULL,
-    Number              INTEGER NOT NULL,
-    WorkOrder          CHAR (10) NOT NULL,
-    PartNumber         CHAR (10) NOT NULL,
-    PartDescription    CHAR (10),
-    ToolLocation       CHAR (10),
-    Comment1           CHAR (10),
-    Comment2           CHAR (10),
-    Comment3           CHAR (10),
-    PartTCs            CHAR (10),
-    PartProbes         CHAR (5),
-    OtherSensors       CHAR (5),
+    FileName           TEXT NOT NULL,
+    Number              INT NOT NULL,
+    WorkOrder          INT NOT NULL,
+    PartNumber         INT NOT NULL,
+    PartDescription    TEXT,
+    ToolLocation       TEXT,
+    Comment1           TEXT,
+    Comment2           TEXT,
+    Comment3           TEXT,
+    PartTCs            TEXT,
+    PartProbes         TEXT,
+    OtherSensors       TEXT,
     PRIMARY KEY (Number)       
     FOREIGN KEY (FileName) REFERENCES RUN_DETAILS(FileName)
 );
