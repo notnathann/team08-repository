a77ee0430edd28ce94f2d3ff120e535e8a525750
Mitchell Simmons <s3530190@student.rmit.edu.au>
2022-11-02 16:09:21 +1100

    Moved database files and modified IndexController

--- src/main/java/group08/boeing/index/IndexController.java
+++ src/main/java/group08/boeing/index/IndexController.java
@@ -3,14 +3,20 @@ package group08.boeing.index;
 import io.javalin.http.Handler;
 import java.util.Map;
 import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class IndexController {
     public static final String HTML_REQUEST = "/";
+    
     public static final String JSON_POST_REQUEST = "/upload";
+    
     public static final String TARGET_HTML_FILE_PATH = "/html/index.html";
-    // public static final String RESOURCE_HEADER_HTML_FILE_PATH = "./src/main/resources/html/common/header.html";
+
+    private static final String DATABASE = "jdbc:sqlite:database/autoclave.db";
 
     // Implement Handler functional interface with lambda expression (acts as override for `handle` abstract method)
     public static Handler servePage = ctx -> {
@@ -25,5 +31,22 @@ public class IndexController {
         Map<String, Object> mapping = new ObjectMapper().readValue(jsonStr, HashMap.class);
         Map<String, Object> runDetails = (Map<String, Object>)mapping.get("RunDetails");
         System.out.println(runDetails.get("FileName"));
+
+        // Connect to JDBC data base
+        Connection connection = DriverManager.getConnection(DATABASE);
+
+        // Prepare INSERT statement
+        String sql = String.format("""
+            INSERT into LGA_Age_Base VALUES (
+                %d, %d, \"%s\", \"%s\", \"%s\", %d
+            );""", 
+            lgaCode, CENSUS_YEAR, INDIGENOUS_STATUS[indexStatus], 
+            SEX[indexSex], AGE_CATEGORY[indexCategory], count
+        );
+
+        // Execute INSERT statement
+        Statement statement = connection.createStatement();
+        System.out.println("Executing: " + sql);
+        statement.execute(sql);
     };
 }
